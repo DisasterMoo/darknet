79e1ea9e15bcda8afc3a9f3d9376ae482b6ca7ab
Luan <luan.santos.pinto@gmail.com>
2018-06-14 17:02:02 -0300

    Output object detection as GPIO signals

--- src/demo.c
+++ src/demo.c
@@ -11,6 +11,12 @@
 
 #define DEMO 1
 
+#define IN  0
+#define OUT 1
+ 
+#define LOW  0
+#define HIGH 1
+
 #ifdef OPENCV
 
 static char **demo_names;
@@ -83,6 +89,164 @@ detection *avg_predictions(network *net, int *nboxes)
     return dets;
 }
 
+/**
+ * GPIO FUNCTIONS
+ */
+int
+GPIOExport(int pin)
+{
+#define BUFFER_MAX 3
+	char buffer[BUFFER_MAX];
+	ssize_t bytes_written;
+	int fd;
+ 
+	fd = open("/sys/class/gpio/export", O_WRONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open export for writing!\n");
+		return(-1);
+	}
+ 
+	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+	write(fd, buffer, bytes_written);
+	close(fd);
+	return(0);
+}
+ 
+int
+GPIOUnexport(int pin)
+{
+	char buffer[BUFFER_MAX];
+	ssize_t bytes_written;
+	int fd;
+ 
+	fd = open("/sys/class/gpio/unexport", O_WRONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open unexport for writing!\n");
+		return(-1);
+	}
+ 
+	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+	write(fd, buffer, bytes_written);
+	close(fd);
+	return(0);
+}
+ 
+int
+GPIODirection(int pin, int dir)
+{
+	static const char s_directions_str[]  = "in\0out";
+ 
+#define DIRECTION_MAX 35
+	char path[DIRECTION_MAX];
+	int fd;
+ 
+	snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
+	fd = open(path, O_WRONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open gpio direction for writing!\n");
+		return(-1);
+	}
+ 
+	if (-1 == write(fd, &s_directions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3)) {
+		fprintf(stderr, "Failed to set direction!\n");
+		return(-1);
+	}
+ 
+	close(fd);
+	return(0);
+}
+ 
+int
+GPIORead(int pin)
+{
+#define VALUE_MAX 30
+	char path[VALUE_MAX];
+	char value_str[3];
+	int fd;
+ 
+	snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+	fd = open(path, O_RDONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open gpio value for reading!\n");
+		return(-1);
+	}
+ 
+	if (-1 == read(fd, value_str, 3)) {
+		fprintf(stderr, "Failed to read value!\n");
+		return(-1);
+	}
+ 
+	close(fd);
+ 
+	return(atoi(value_str));
+}
+ 
+int
+GPIOWrite(int pin, int value)
+{
+	static const char s_values_str[] = "01";
+ 
+	char path[VALUE_MAX];
+	int fd;
+ 
+	snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+	fd = open(path, O_WRONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open gpio value for writing!\n");
+		return(-1);
+	}
+ 
+	if (1 != write(fd, &s_values_str[LOW == value ? 0 : 1], 1)) {
+		fprintf(stderr, "Failed to write value!\n");
+		return(-1);
+	}
+ 
+	close(fd);
+	return(0);
+}
+
+
+/**
+ * TO-DO: Think how to drive car based on newest detections and *last decisions*
+ */
+void doDriveCar(detection *dets, int num)
+{
+	//Width and Height for detection, Higher = more accuracy
+        int wid, hei, div1, div2;
+	wid = 1000;
+	hei = 1000;
+	//Divisions
+	div1 = wid/3;
+	div2 = wid*2/3;
+	int leftCrash, middleCrash, rightCrash;
+	leftCrash = 0;
+	middleCrash = 0;
+	rightCrash = 0;
+	int i;
+	for(i = 0; i < num; ++i){
+		box b = dets[i].bbox;
+		int left  = (b.x-b.w/2.)*wid;
+		int right = (b.x+b.w/2.)*wid;
+		int top   = (b.y-b.h/2.)*hei; //UNUSED FOR NOW
+		int bot   = (b.y+b.h/2.)*hei; //UNUSED FOR NOW
+		if(left < div1){
+			leftCrash = 1;
+			if(right > div1)middleCrash = 1;
+			if(right > div2)rightCrash = 1;
+		}else if(left < div2){
+			middleCrash = 1;
+			if(right > div2)rightCrash = 1;		
+		}else{
+			rightCrash = 1;
+		}
+	}
+	printf("LEFT: %d\nMIDDLE: %d\nRIGHT: %d\n", leftCrash, middleCrash, rightCrash);
+	//SET GPIO OUTPUT
+	GPIOWrite(219, leftCrash);
+	GPIOWrite(186, middleCrash);
+	GPIOWrite(63, rightCrash);	
+}
+
 void *detect_in_thread(void *ptr)
 {
     running = 1;
@@ -131,6 +295,7 @@ void *detect_in_thread(void *ptr)
     printf("Objects:\n\n");
     image display = buff[(buff_index+2) % 3];
     draw_detections(display, dets, nboxes, demo_thresh, demo_names, demo_alphabet, demo_classes);
+    doDriveCar(dets, nboxes);
     free_detections(dets, nboxes);
 
     demo_index = (demo_index + 1)%demo_frame;
@@ -184,6 +349,13 @@ void *detect_loop(void *ptr)
 
 void demo(char *cfgfile, char *weightfile, float thresh, int cam_index, const char *filename, char **names, int classes, int delay, char *prefix, int avg_frames, float hier, int w, int h, int frames, int fullscreen)
 {
+    //SET GPIO PINOUT
+    GPIOExport(219); //LEFT
+    GPIOExport(186); //MIDDLE
+    GPIOExport(63); //RIGHT
+    GPIODirection(219, 1);
+    GPIODirection(186, 1);
+    GPIODirection(63, 1);
     //demo_frame = avg_frames;
     image **alphabet = load_alphabet();
     demo_names = names;
@@ -242,19 +414,17 @@ void demo(char *cfgfile, char *weightfile, float thresh, int cam_index, const ch
         buff_index = (buff_index + 1) %3;
         if(pthread_create(&fetch_thread, 0, fetch_in_thread, 0)) error("Thread creation failed");
         if(pthread_create(&detect_thread, 0, detect_in_thread, 0)) error("Thread creation failed");
-        if(!prefix){
-            fps = 1./(what_time_is_it_now() - demo_time);
-            demo_time = what_time_is_it_now();
-            display_in_thread(0);
-        }else{
-            char name[256];
-            sprintf(name, "%s_%08d", prefix, count);
-            save_image(buff[(buff_index + 1)%3], name);
-        }
+        fps = 1./(what_time_is_it_now() - demo_time);
+        demo_time = what_time_is_it_now();
+        display_in_thread(0);
         pthread_join(fetch_thread, 0);
         pthread_join(detect_thread, 0);
         ++count;
     }
+    //RESET GPIO
+    GPIOUnexport(219);
+    GPIOUnexport(186);
+    GPIOUnexport(63);
 }
 
 /*
--- src/demo.h
+++ src/demo.h
@@ -2,5 +2,11 @@
 #define DEMO_H
 
 #include "image.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #endif
